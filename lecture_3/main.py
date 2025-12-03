9635501b32d37eb9bae0c7f3a251c5f19cd2f4fd
Victoria <almost.a.mathematician@gmail.com>
2025-12-03 13:36:15 +0300

    fix lecture_3 using pylint

--- lecture_3/main.py
+++ lecture_3/main.py
@@ -1,3 +1,4 @@
+'''Students grade manager'''
 from typing import Any, Set
 from re import split
 
@@ -20,22 +21,23 @@ def add_student() -> None:
     if not name.replace(' ', '').isalpha():
         print('Name should be not empty and contain only alphabetic characters')
         return
-        
+
     # Check if student exists in index (O(1) complexity). If student already exists - don't add
     if name in student_index:
         print('Student with this name already exists')
         return
-    
+
     # Add student to students list and add their name to student_index
     new_student = {'name': name, 'grades': [], 'average': None}
     students.append(new_student)
     student_index.add(new_student['name'])
 
 
-def add_student_grade() -> None: 
+def add_student_grade() -> None:
     '''
     This function adds one or more grades for a student by his name
-    Supports entering a single grade or multiple grades at once, with 'done' input stops the operation
+    Supports entering a single grade or multiple grades at once, 
+    with 'done' input stops the operation
     EXAMPLES:
     Input: 80
     Result: grade 80 is added, input is requested again
@@ -60,14 +62,16 @@ def add_student_grade() -> None:
     # Notify the user and stop function execution if student was not found by name
     if found_student is None:
         print('Student with this name does not exist')
-        return 
-    
+        return
+
     # Start infinite input loop
     while True:
         done_entered: bool = False
 
         # Accept user input, normalize and split it by delimiters
-        grades_input: list[str] = split(r'[,;\s]+', input('Enter a grade (or \'done\' to finish): ').strip().lower())
+        grades_input: list[str] = split(
+            r'[,;\s]+', input('Enter a grade (or \'done\' to finish): ').strip().lower()
+        )
 
         # If 'done' is among the elements, trim the list including the found string itself
         if grades_input.count('done'):
@@ -79,7 +83,7 @@ def add_student_grade() -> None:
             # Stop the function early if grades list is empty after excluding 'done'
             if not grades_input:
                 break
-        
+
         # Start the loop through list to validate its elements
         for grade in grades_input:
             # Try to convert element to int. Loop will break on failure
@@ -104,11 +108,12 @@ def add_student_grade() -> None:
             # If 'done' was entered, stop function execution
             if done_entered:
                 break
-    
+
 
 def get_report() -> None:
     '''
-    This function displays a detailed summary of all students' grades and the minimum, maximum, and overall average grade
+    This function displays a detailed summary of all students' grades
+    and the minimum, maximum, and overall average grade
     among all students who have at least one grade
     '''
 
@@ -134,7 +139,7 @@ def get_report() -> None:
         except ZeroDivisionError:
             # In case of ZeroDivisionError, record status indicating no grade
             average: str = 'N/A'
-        
+
         print(f'{student["name"]}\'s average grade is {average}.')
 
 
@@ -145,36 +150,43 @@ def get_report() -> None:
         else:
             # Display a message about absence of assigned grades
             print('There is no grades')
-        
+
     else:
         # Output average grades for all the students
         print('--------------------------')
         print(f'Max Average: {max(average_list):.1f}')
-        print(f'Min Average: {min(average_list):.1f}') 
+        print(f'Min Average: {min(average_list):.1f}')
         print(f'Overall Average: {(sum(average_list)/len(average_list)):.1f}')
 
 
 def get_top_performer() -> None:
     '''
-    This function calculates the student with the highest average grade among all others and displays a message
-    about him.
+    This function calculates the student with the highest average grade among all others 
+    and displays a message about him.
     '''
 
     # Create a filtered list of students where each student has an average grade
-    graded_students: list[dict] = list(filter(lambda student: student['average'] != None, students))
+    graded_students: list[dict] = list(
+        filter(lambda student: student['average'] is not None, students)
+    )
 
     # Select a student with the highest average grade. Returns None if students list is empty
-    top_performer: dict | None = max(graded_students, default=None, key=lambda student: student['average'])
+    top_performer: dict | None = max(
+        graded_students, default=None, key=lambda student: student['average']
+    )
 
     # Display an appropriate message
     if top_performer is None:
         print('There is no student with the highest average grade')
     else:
-        print(f'The student with the highest average is {top_performer["name"]} with a grade of {top_performer["average"]:.1f}')
+        print(
+            f'The student with the highest average is {top_performer["name"]} \
+            with a grade of {top_performer["average"]:.1f}'
+        )
 
 
 # The main program loop
-while True: 
+while True:
     print('\n--- Student Grade Analyzer ---')
     print('1. Add a new student')
     print('2. Add grades for a student')
